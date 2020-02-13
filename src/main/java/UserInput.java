9479baf36a977521da28ff412d6b43ad165ae940
Miguel Vera <miguel.vera7908@gmail.com>
2020-02-13 14:03:00 -0600

    Finished Dependency Management Exercise

---
+++ src/main/java/UserInput.java
@@ -0,0 +1,31 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Something: ");
+
+        String userInput = scanner.nextLine();
+
+        System.out.println("You Entered: " + userInput);
+
+        if(StringUtils.isNumeric(userInput)){
+            System.out.println(userInput + " is a number.");
+        }else{
+            System.out.println(userInput + " is not a number.");
+        }
+
+        System.out.println( "Flipped Case: " + StringUtils.swapCase(userInput));
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+
+
+
+    }
+
+
+}
